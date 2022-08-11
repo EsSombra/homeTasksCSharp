e8a815b0ae92b4bb66c60e8f2f0114a79bfee99d
Natalia <109825373+EsSombra@users.noreply.github.com>
2022-08-11 23:20:19 +0300

    description added

--- example2/Program.cs
+++ example2/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.WriteLine("Введите число");
+//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.WriteLine("Введите число");
 double num1 = Convert.ToDouble(Console.ReadLine());
 System.Console.WriteLine("Введите второе число");
 double num2 = Convert.ToDouble(Console.ReadLine());
