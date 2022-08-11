28a8c7f788f94ff00396bef3102edc0a23c75d62
Ata <nigilistkaa@gmail.com>
2022-08-11 18:38:12 +0300

    changed to double

--- example2/Program.cs
+++ example2/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.WriteLine("Введите число");
-int num1 = Convert.ToInt32(Console.ReadLine());
+double num1 = Convert.ToDouble(Console.ReadLine());
 System.Console.WriteLine("Введите второе число");
-int num2 = Convert.ToInt32(Console.ReadLine());
+double num2 = Convert.ToDouble(Console.ReadLine());
 
 if (num1 > num2)
 {
