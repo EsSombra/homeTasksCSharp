97e88b451e93c80aa473a5a2e810e4135899a879
Ata <nigilistkaa@gmail.com>
2022-08-11 23:15:13 +0300

    add example8(4)

---
+++ example8/Program.cs
@@ -0,0 +1,20 @@
+﻿System.Console.WriteLine("Введите число");
+double num = Convert.ToDouble(Console.ReadLine());
+int count = 1;
+int evenNum = 0;
+
+System.Console.WriteLine("чётные числа от 1 до " + num + ": ");
+while (count <= num)
+    {
+        if (count%2 == 0)
+        {
+            evenNum = count;
+            System.Console.WriteLine(evenNum);
+            count++;
+        }    
+        else
+        {
+            count++;
+        }
+    }
+
