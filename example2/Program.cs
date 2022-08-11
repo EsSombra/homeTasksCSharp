f0dac1bfb942afaf94026026f132cfc3456d1d73
Ata <nigilistkaa@gmail.com>
2022-08-11 15:56:58 +0300

    add example2(1)

---
+++ example2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+{
+    System.Console.WriteLine("Большее число " + num1 + ", меньшее число " + num2);
+}
+if (num1 == num2)
+{
+    System.Console.WriteLine("Числа равны");
+}
+else
+{
+    System.Console.WriteLine("Большее число " + num2 + ", меньшее число " + num1);
+}
