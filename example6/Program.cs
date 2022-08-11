f87fa7a2c7c9db308109ad4e1a0ae2985b9c26dc
Ata <nigilistkaa@gmail.com>
2022-08-11 21:37:53 +0300

    add example6(3)

---
+++ example6/Program.cs
@@ -0,0 +1,10 @@
+﻿System.Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num%2 == 0)
+{
+    System.Console.WriteLine(num + " является чётным");
+}
+else
+{
+    System.Console.WriteLine(num + " является нечётным");
+}
