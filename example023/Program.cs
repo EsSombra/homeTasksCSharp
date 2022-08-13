83fc258c1f179017aa81e97b7a9a8d7c1fa954dc
Ata <nigilistkaa@gmail.com>
2022-08-14 01:36:11 +0300

    add #23(3)

---
+++ example023/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+
+Console.WriteLine("Enter your number, please");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Third power of digitals from 1 to {num} are: ");
+for (int count = 1; count<=num;count++)
+{
+    Console.Write(Math.Pow(count,3) + "\t");
+}
