ab2ab5a11cd44b94439b6eed086bf7c180fa54f0
Ata <nigilistkaa@gmail.com>
2022-08-13 12:57:03 +0300

    add example015(3)

---
+++ example015/Program.cs
@@ -0,0 +1,8 @@
+﻿//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+System.Console.WriteLine("Введите число");
+int day = Convert.ToInt32(Console.ReadLine());
+int dayOff1 = 6, dayOff2 = 7, daysPerWeek = 7; // магическим числам - бой!)
+if (day == dayOff1 || day == dayOff2) System.Console.WriteLine($"Ура! {day} день недели - выходной!");
+if (day <= daysPerWeek && day != dayOff1 && day != dayOff2) System.Console.WriteLine($"{day} недели - рабочий...");
+if (day > daysPerWeek) System.Console.WriteLine("Такого дня недели нет.");
