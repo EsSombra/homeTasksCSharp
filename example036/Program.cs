3074800494d177e0369a635a049e068232a03bc9
Ata <nigilistkaa@gmail.com>
2022-08-23 22:23:53 +0300

    №36 (2) fixed

--- example036/Program.cs
+++ example036/Program.cs
@@ -3,12 +3,13 @@
 Console.WriteLine("Enter array size, please:");
 int[] array = new int[Convert.ToInt32(Console.ReadLine())];
 int size = array.Length;
+int sum = 0;
 for (int i = 0; i < size; i++)
 {
     array[i] = new Random().Next(-100, 101);
     Console.Write(array[i] + "; ");
 }
-int sum = 0;
+
 for (int i = 1; i < size; i += 2)
 {
     sum += array[i];
