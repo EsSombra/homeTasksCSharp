f7879bc96a55d78657b91bff69e987ea779de529
Ata <nigilistkaa@gmail.com>
2022-08-23 22:03:05 +0300

    example034(1)

---
+++ example034/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве
+
+int [] list = new int [13];
+int size = list.Length;
+int count = 0;
+for (int i = 0; i < size; i++)
+{
+    list [i] = new Random().Next(100,1000);
+    if (list [i] %2 == 0) count++;
+}
+Console.WriteLine($"Count of even numbers of [{string.Join("; ", list)}] is {count}");
